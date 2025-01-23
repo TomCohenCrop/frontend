818f3ae2b699266f4536e2bea700ca5e47314982
eyalyehiely <eyalwork0@gmail.com>
2025-01-23 22:11:33 +0200

    `Update vite.config.ts: remove import statements, reorder and add newline at end of file`

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
-    host: true // Enable network access
+    host: true,
+    port: process.env.PORT || 3000
   },
   preview: {
-    port: process.env.PORT || 3000,
-    host: true
+    host: true,
+    port: process.env.PORT || 3000
   }
 })
